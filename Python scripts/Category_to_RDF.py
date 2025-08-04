a0ca2f3201c26db152d86a563aee670f1aac023d
argylaz <argyrhslazaridhs@gmail.com>
2025-08-04 18:07:16 +0300

    Added some comments to the templates - changed mappin class type prefix

--- Python scripts/Category_to_RDF.py
+++ Python scripts/Category_to_RDF.py
@@ -73,124 +73,124 @@ g.add((road_sign, RDFS.subClassOf, mappin_base_class))
 g.add((road_sign, RDFS.label, Literal("Road Sign")))
 
 # Define specific mappin types as subclasses
-g.add((dbr.Blacksmith, RDFS.subClassOf, crafting_station))
-g.add((dbr.Blacksmith, OWL.sameAs, witcher.Blacksmiths))    # Link to the main Blacksmith class
-g.add((dbr.Armorer, RDFS.subClassOf, crafting_station))     
-g.add((dbr.Armorer, OWL.sameAs, witcher.Armorers))          # Link to the main Armorer class
-g.add((dbr.Whetstone, RDFS.subClassOf, crafting_station))
-g.add((dbr.AlchemyTable, RDFS.subClassOf, crafting_station))
-g.add((dbr.Grindstone, RDFS.subClassOf, witcher.Whetstone)) # A Grindstone is a type of Whetstone
+g.add((witcher.Blacksmith, RDFS.subClassOf, crafting_station))
+g.add((witcher.Blacksmith, OWL.sameAs, witcher.Blacksmiths))    # Link to the main Blacksmith class
+g.add((witcher.Armorer, RDFS.subClassOf, crafting_station))     
+g.add((witcher.Armorer, OWL.sameAs, witcher.Armorers))          # Link to the main Armorer class
+g.add((witcher.Whetstone, RDFS.subClassOf, crafting_station))
+g.add((witcher.AlchemyTable, RDFS.subClassOf, crafting_station))
+g.add((witcher.Grindstone, RDFS.subClassOf, witcher.Whetstone)) # A Grindstone is a type of Whetstone
 
 # Define specific mappin types
-g.add((dbr.NoticeBoard, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.NoticeBoard, RDFS.label, Literal("Notice Board")))
-g.add((dbr.NoticeBoard, OWL.sameAs, witcher.The_Witcher_3_notice_boards))
+g.add((witcher.NoticeBoard, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.NoticeBoard, RDFS.label, Literal("Notice Board")))
+g.add((witcher.NoticeBoard, OWL.sameAs, witcher.The_Witcher_3_notice_boards))
 
-g.add((dbr.SideQuest, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.SideQuest, RDFS.label, Literal("Side Quest")))
-g.add((dbr.SideQuest, OWL.sameAs, witcher.The_Witcher_3_secondary_quests))
+g.add((witcher.SideQuest, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.SideQuest, RDFS.label, Literal("Side Quest")))
+g.add((witcher.SideQuest, OWL.sameAs, witcher.The_Witcher_3_secondary_quests))
 
-g.add((dbr.PlaceOfPower, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.PlaceOfPower, RDFS.label, Literal("Place of Power")))
+g.add((witcher.PlaceOfPower, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.PlaceOfPower, RDFS.label, Literal("Place of Power")))
 
-g.add((dbr.MonterNest, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.MonterNest, RDFS.subClassOf, mappin_base_class))
 g.add((dbr.MonterNest, RDFS.label, Literal("Monster Nest")))
 
-g.add((dbr.Harbor, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.Harbor, RDFS.label, Literal("Harbor")))
+g.add((witcher.Harbor, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.Harbor, RDFS.label, Literal("Harbor")))
 
-g.add((dbr.BanditCampfire, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.BanditCampfire, RDFS.label, Literal("Bandit Campfire")))
+g.add((witcher.BanditCampfire, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.BanditCampfire, RDFS.label, Literal("Bandit Campfire")))
 
-g.add((dbr.Contraband, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.Contraband, RDFS.label, Literal("Contraband")))
+g.add((witcher.Contraband, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.Contraband, RDFS.label, Literal("Contraband")))
 
-g.add((dbr.Entrance, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.Entrance, RDFS.label, Literal("Entrance")))
+g.add((witcher.Entrance, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.Entrance, RDFS.label, Literal("Entrance")))
 
-g.add((dbr.Merchant, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.Merchant, RDFS.label, Literal("Merchant")))
-g.add((dbr.Merchant, OWL.sameAs, witcher.Merchants))
+g.add((witcher.Merchant, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.Merchant, RDFS.label, Literal("Merchant")))
+g.add((witcher.Merchant, OWL.sameAs, witcher.Merchants))
 
-g.add((dbr.PlayerStash, RDFS.subClassOf, mappin_base_class))  
-g.add((dbr.PlayerStash, RDFS.label, Literal("Player Stash")))
+g.add((witcher.PlayerStash, RDFS.subClassOf, mappin_base_class))  
+g.add((witcher.PlayerStash, RDFS.label, Literal("Player Stash")))
 
-g.add((dbr.RescuingTown, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.RescuingTown, RDFS.label, Literal("Rescuing Town")))
+g.add((witcher.RescuingTown, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.RescuingTown, RDFS.label, Literal("Rescuing Town")))
 
-g.add((dbr.SpoilsOfWar, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.SpoilsOfWar, RDFS.label, Literal("Spoils of War")))
+g.add((witcher.SpoilsOfWar, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.SpoilsOfWar, RDFS.label, Literal("Spoils of War")))
 
-g.add((dbr.TreasureHuntMappin, RDFS.subClassOf, mappin_base_class))   
-g.add((dbr.TreasureHuntMappin, RDFS.label, Literal("Treasure Hunt Map Pin")))
+g.add((witcher.TreasureHuntMappin, RDFS.subClassOf, mappin_base_class))   
+g.add((witcher.TreasureHuntMappin, RDFS.label, Literal("Treasure Hunt Map Pin")))
 
-g.add((dbr.TreasureQuest, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.TreasureQuest, RDFS.label, Literal("Treasure Quest")))
-g.add((dbr.TreasureQuest, OWL.sameAs, witcher.The_Witcher_3_treasure_hunts))
+g.add((witcher.TreasureQuest, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.TreasureQuest, RDFS.label, Literal("Treasure Quest")))
+g.add((witcher.TreasureQuest, OWL.sameAs, witcher.The_Witcher_3_treasure_hunts))
 
-g.add((dbr.ArmorRepairTable, RDFS.subClassOf, crafting_station))
-g.add((dbr.ArmorRepairTable, RDFS.label, Literal("Armor Repair Table")))
+g.add((witcher.ArmorRepairTable, RDFS.subClassOf, crafting_station))
+g.add((witcher.ArmorRepairTable, RDFS.label, Literal("Armor Repair Table")))
 
-g.add((dbr.BanditCampfire, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.BanditCampfire, RDFS.label, Literal("Bandit Camp Fire")))
+g.add((witcher.BanditCampfire, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.BanditCampfire, RDFS.label, Literal("Bandit Camp Fire")))
 
-g.add((dbr.BossAndTreasure, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.BossAndTreasure, RDFS.label, Literal("Boss And Treasure")))
+g.add((witcher.BossAndTreasure, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.BossAndTreasure, RDFS.label, Literal("Boss And Treasure")))
 
-g.add((dbr.ChapterQuest, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.ChapterQuest, RDFS.label, Literal("Chapter Quest")))
+g.add((witcher.ChapterQuest, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.ChapterQuest, RDFS.label, Literal("Chapter Quest")))
 
-g.add((dbr.DungeonCrawl, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.DungeonCrawl, RDFS.label, Literal("DungeonCrawl")))
+g.add((witcher.DungeonCrawl, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.DungeonCrawl, RDFS.label, Literal("DungeonCrawl")))
 
-g.add((dbr.Enchant, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.Enchant, RDFS.label, Literal("Enchant")))
+g.add((witcher.Enchant, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.Enchant, RDFS.label, Literal("Enchant")))
 
-g.add((dbr.GwentPlayer, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.GwentPlayer, RDFS.label, Literal("Gwent Player")))
-g.add((dbr.GwentPlayer, RDFS.subClassOf, witcher.Gwent))  # Link to the main Gwent class
+g.add((witcher.GwentPlayer, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.GwentPlayer, RDFS.label, Literal("Gwent Player")))
+g.add((witcher.GwentPlayer, RDFS.subClassOf, witcher.Gwent))  # Link to the main Gwent class
 
-g.add((dbr.GwentSeller, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.GwentSeller, RDFS.label, Literal("Gwent Seller")))
-g.add((dbr.GwentSeller, RDFS.subClassOf, witcher.Gwent))  # Link to the main Gwent class
+g.add((witcher.GwentSeller, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.GwentSeller, RDFS.label, Literal("Gwent Seller")))
+g.add((witcher.GwentSeller, RDFS.subClassOf, witcher.Gwent))  # Link to the main Gwent class
 
-g.add((dbr.Herbalist, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.Herbalist, RDFS.label, Literal("Herbalist")))
-g.add((dbr.Herbalist, OWL.sameAs, witcher.Herbalists))  # Link to the main Herbalist class
+g.add((witcher.Herbalist, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.Herbalist, RDFS.label, Literal("Herbalist")))
+g.add((witcher.Herbalist, OWL.sameAs, witcher.Herbalists))  # Link to the main Herbalist class
 
-g.add((dbr.HorseRacing, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.HorseRacing, RDFS.label, Literal("Horse Racing")))
+g.add((witcher.HorseRacing, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.HorseRacing, RDFS.label, Literal("Horse Racing")))
 
-g.add((dbr.MonsterQuest, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.MonsterQuest, RDFS.label, Literal("Monster Quest")))
+g.add((witcher.MonsterQuest, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.MonsterQuest, RDFS.label, Literal("Monster Quest")))
 
-g.add((dbr.Teleport, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.Teleport, RDFS.label, Literal("Teleport")))
+g.add((witcher.Teleport, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.Teleport, RDFS.label, Literal("Teleport")))
 
-g.add((dbr.Bed, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.Bed, RDFS.label, Literal("Bed")))
+g.add((witcher.Bed, RDFS.subClassOf, mappin_base_class))
+g.adwitcherwitcher.Bed, RDFS.label, Literal("Bed")))
 
-g.add((dbr.Hideout, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.Hideout, RDFS.label, Literal("Hideout")))
+g.add((witcher.Hideout, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.Hideout, RDFS.label, Literal("Hideout")))
 
-g.add((dbr.InfestedVineyard, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.InfestedVineyard, RDFS.label, Literal("Infested Vineyard")))
-g.add((dbr.InfestedVineyard, RDFS.subClassOf, witcher.Vineyards))
+g.add((witcher.InfestedVineyard, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.InfestedVineyard, RDFS.label, Literal("Infested Vineyard")))
+g.add((witcher.InfestedVineyard, RDFS.subClassOf, witcher.Vineyards))
 
-g.add((dbr.KnightErrant, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.KnightErrant, RDFS.label, Literal("Knight Errant")))
+g.add((witcher.KnightErrant, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.KnightErrant, RDFS.label, Literal("Knight Errant")))
 
-g.add((dbr.MutagenDismantle, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.MutagenDismantle, RDFS.label, Literal("Mutagen Dismantle")))
+g.add((witcher.MutagenDismantle, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.MutagenDismantle, RDFS.label, Literal("Mutagen Dismantle")))
 
-g.add((dbr.Plegmund, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.Plegmund, RDFS.label, Literal("Plegmund")))
+g.add((witcher.Plegmund, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.Plegmund, RDFS.label, Literal("Plegmund")))
 
-g.add((dbr.SignalingStake, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.SignalingStake, RDFS.label, Literal("Signaling Stake")))
+g.add((witcher.SignalingStake, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.SignalingStake, RDFS.label, Literal("Signaling Stake")))
 
-g.add((dbr.WineContract, RDFS.subClassOf, mappin_base_class))
-g.add((dbr.WineContract, RDFS.label, Literal("Wine Contract")))
+g.add((witcher.WineContract, RDFS.subClassOf, mappin_base_class))
+g.add((witcher.WineContract, RDFS.label, Literal("Wine Contract")))
 
 
 # Link game concepts (professions) to the mappin types they use
